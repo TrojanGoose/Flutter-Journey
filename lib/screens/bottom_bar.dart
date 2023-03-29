cccca0eb9cce30d5e3010818d8aae6b9b8ad60ae
TrojanGoose <godwin68567@gmail.com>
2023-03-29 22:23:41 +0000

    added search bar and styles

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -10,7 +10,7 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
     const HomeScreen(),
     const Text('Search'),
@@ -27,9 +27,6 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Tickets'),
-      ),
       body: Center(
         child: _widgetOptions[_selectedIndex],
       ),
