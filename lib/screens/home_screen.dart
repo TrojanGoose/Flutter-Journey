e61c09691390b86ed27fc65d9e25a0dd0847b2fb
TrojanGoose <godwin68567@gmail.com>
2023-03-31 23:25:45 +0000

    added flight card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bookticket/screens/ticketview.dart';
 import 'package:bookticket/utils/app.styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
@@ -50,7 +51,8 @@ class HomeScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: const Color(0xFFF4F6FD)),
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   child: Row(
                     children: [
                       const Icon(FluentSystemIcons.ic_fluent_search_regular,
@@ -61,10 +63,30 @@ class HomeScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Upcoming Flights',
+                      style: Styles.headLineStyle2,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'View All',
+                        style: Styles.textStyle
+                            .copyWith(color: Styles.primaryColor),
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
-          )
+          ),
+          const Gap(15),
+          TicketView(),
         ],
       ),
     );
